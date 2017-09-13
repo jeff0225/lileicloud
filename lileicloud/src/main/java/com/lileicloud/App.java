729314eaa19d5bda57f8f59a2b3440c07b9427e1
jeff0225 <lileihill@163.com>
2017-09-13 09:29:43 +0800

    First commit

---
+++ lileicloud/src/main/java/com/lileicloud/App.java
@@ -0,0 +1,13 @@
+package com.lileicloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lileicloud/user/src/main/java/user/lileicloud/App.java
@@ -0,0 +1,13 @@
+package user.lileicloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "启动用户中心" );
+    }
+}
